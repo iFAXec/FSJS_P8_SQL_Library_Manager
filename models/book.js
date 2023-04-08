6e35927fa949a95cbae6f0611a6d0fceb67dbb8b
ifaxec <xecfahim.ahmed@gmail.com>
2023-04-08 23:52:25 +0100

    Updated index.js

--- models/book.js
+++ models/book.js
@@ -13,6 +13,13 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Book.init({
+
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+
     title: {
       type: DataTypes.STRING,
       allowNull: false,
--- routes/index.js
+++ routes/index.js
@@ -3,45 +3,63 @@ var router = express.Router();
 const { Book } = require("../models")
 
 
+/* handler function to wrap each route */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+
+    } catch (err) {
+      res.render("error", { error: err })
+    }
+  }
+}
+
+
+
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/', asyncHandler(async function (req, res, next) {
 
   const books = await Book.findAll();
-  // console.log('books :', books);
   res.json(books);
-
-  // res.render('index', { title: 'Express' });
-});
+  // res.render("index", { title: "Books", books: Book })
+}));
 
 
 /* GET  - Shows the full list of books */
-router.get("/books", async (req, res) => {
+router.get("/books", asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [["id", "DESC"]] })
+  res.render();
 
-})
+}))
+
+
 
 /* GET - Show the create new book form  */
-router.get("/books/new", async (req, res) => {
+router.get("/books/new", asyncHandler(async (req, res) => {
 
-})
+}))
 
 /* POST- New book to the database */
-router.post("/books/new", async (req, res) => {
-
-})
+router.post("/books/new", asyncHandler(async (req, res) => {
+  const book = await Book.create(req.body);
+  // console.log(req.body);
+  res.redirect("/new-book/")
+}))
 
 /*GET - Shows book detail form */
-router.get("books/:id", async (req, res) => {
+router.get("books/:id", asyncHandler(async (req, res) => {
 
-})
+}))
 
 /*POST - Update book info in the database */
-router.post("books/:id", async (req, res) => {
+router.post("books/:id", asyncHandler(async (req, res) => {
 
-})
+}))
 
 /*POST - Deletes a book */
-router.post("/books/:id/delete", async (req, res) => {
+router.post("/books/:id/delete", asyncHandler(async (req, res) => {
 
-})
+}))
 
 module.exports = router;
