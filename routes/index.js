9f48467547a13866221ca1161f16bc94148ec878
ifaxec <xecfahim.ahmed@gmail.com>
2023-04-10 13:21:35 +0100

    Updated pug templates

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { Book } = require("../models")
+const { Book } = require("../models");
 
 
 /* handler function to wrap each route */
@@ -19,24 +19,24 @@ function asyncHandler(cb) {
 
 /* GET home page. */
 router.get('/', asyncHandler(async function (req, res, next) {
-
   const books = await Book.findAll();
-  res.json(books);
-  // res.render("index", { title: "Books", books: Book })
+  // res.json(books);
+  res.render("index", { books: books, title: "Books" })
 }));
 
 
 /* GET  - Shows the full list of books */
 router.get("/books", asyncHandler(async (req, res) => {
   const books = await Book.findAll({ order: [["id", "DESC"]] })
-  res.render();
-
+  res.render("index", { books: books, title: "Books" });
 }))
 
 
 
 /* GET - Show the create new book form  */
 router.get("/books/new", asyncHandler(async (req, res) => {
+  const book = await Book.create(req.body)
+  res.render();
 
 }))
 
@@ -48,12 +48,14 @@ router.post("/books/new", asyncHandler(async (req, res) => {
 }))
 
 /*GET - Shows book detail form */
-router.get("books/:id", asyncHandler(async (req, res) => {
+router.get("/books/:id", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.parms.id)
+  res.render("books/show", { book: book, title: "New Book" })
 
 }))
 
 /*POST - Update book info in the database */
-router.post("books/:id", asyncHandler(async (req, res) => {
+router.post("/books/:id", asyncHandler(async (req, res) => {
 
 }))
 
